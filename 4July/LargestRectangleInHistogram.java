bfb1f2081f961a9beff2c5c3fdd2c05cd68489c9
Ashvani Sharma <panditashuabvp@gmail.com>
2025-07-04 23:54:08 +0530

    Histogram

---
+++ 4July/LargestRectangleInHistogram.java
@@ -0,0 +1,7 @@
+public class LargestRectangleInHistogram{
+    public static void main(String[] args) {
+        int[] heights = {2,1,5,6,2,3};
+        int pse[]=new int[heights.length];
+        int nse[]=new int[heights.length];
+    }
+}
